6e1fbc763a243c9bbfec8e66b2c8fb591e646e73
kevin1232sn1 <k.bos@gmx.com>
2017-04-04 21:11:51 +0200

    Revert "Internationalization/pagination/structured debugging merg met main"

--- src/main/java/avans/ivh11a1/facturatie/Server.java
+++ src/main/java/avans/ivh11a1/facturatie/Server.java
@@ -2,17 +2,6 @@ package avans.ivh11a1.facturatie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 /**
  * This class is used to run the Spring Boot application. It will start a tomcat server to load all the template files.
@@ -20,30 +9,10 @@ import java.util.Locale;
  * You can find the server on default: http://localhost:8080.
  *
  */
-@EnableSpringDataWebSupport
 @SpringBootApplication
-public class Server extends WebMvcConfigurerAdapter {
+public class Server {
 
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
-    }
-
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("en"));
-        return slr;
-    }
-
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
     }
 }
